18838f70b6f42a5779909765f1c9ba847a586f7a
SStanescu <stanescu_n_stefan@yahoo.com>
2022-02-26 11:36:38 +0200

    - create migrations

---
+++ migrations/Version20220226093342.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220226093342 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('
+            CREATE TABLE car (
+                id INT AUTO_INCREMENT NOT NULL,
+                passenger_no INT NOT NULL, 
+                model VARCHAR(255) DEFAULT NULL, 
+                brand VARCHAR(255) DEFAULT NULL, 
+                price DOUBLE PRECISION NOT NULL, 
+                is_sold TINYINT(1) NOT NULL, 
+                PRIMARY KEY(id)
+            ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        ');
+        $this->addSql('
+            CREATE TABLE truck (
+                id INT AUTO_INCREMENT NOT NULL,
+                pollution_certificate VARCHAR(1) NOT NULL,
+                model VARCHAR(255) DEFAULT NULL,
+                brand VARCHAR(255) DEFAULT NULL,
+                price DOUBLE PRECISION NOT NULL,
+                is_sold TINYINT(1) NOT NULL, PRIMARY KEY(id)
+            ) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        ');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE car');
+        $this->addSql('DROP TABLE truck');
+    }
+}
